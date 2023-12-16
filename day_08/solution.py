ba5d785cf81a2fcfd5f31385f36d7e518e9958de
eo <eo@pop-os.localdomain>
2023-12-16 17:24:33 -0500

    Day 8 part 2! Had to make a seemingly absurd assumption, based on comments online. Not sure this solution works for all data inputs...

--- day_08/solution.py
+++ day_08/solution.py
@@ -69,22 +69,67 @@ else:
 # ---------------------------------------------------------------------------------------------------------------------
 #%% Solution part 2
 
-if input_select == 0:
-    print("", "WARNING: Part 2 solution takes forever", sep="\n", flush = True)
-
 # Get all nodes that end with 'A'
-next_nodes_set = set(node for node in node_lut.keys() if str(node).endswith("A"))
+start_nodes_set = set(node for node in node_lut.keys() if str(node).endswith("A"))
 
-# Loop through rl sequence as before, but map multiple nodes forward
-num_steps = None
-for step_idx, each_char in enumerate(cycle(rl_str)):
+# Run each node through to target (end with 'Z')
+# -> If we hit the target after an integer number of rl_str loops, record the step count
+# -> We'll make a wild assumption that all nodes meet at the end of full-integer rl_str loops
+# -> Then we can find the 'least-common-multiple' of these steps counts to get answer
+rl_length = len(rl_str)
+cycle_count_per_start_node = {}
+for each_start_node in start_nodes_set:
     
-    # Map every node for given character and stop once they all end with 'Z'
-    next_nodes_set = set(node_lut[each_node][each_char] for each_node in next_nodes_set)
-    is_done = all(node.endswith("Z") for node in next_nodes_set)
-    if is_done:
+    next_node = each_start_node
+    num_steps = None
+    for step_idx, each_char in enumerate(cycle(rl_str)):
+        
+        # Check for end condition of each starting node
+        next_node = node_lut[next_node][each_char]
+        if not next_node.endswith("Z"):
+            continue
+        
+        # Now that we've hit an end node, check if we've stepped through the rl string an integer number of times
         num_steps = step_idx + 1
+        is_mult_of_rl_str = (num_steps % rl_length) == 0
+        if is_mult_of_rl_str:
+            cycle_count_per_start_node[each_start_node] = num_steps
+            break
+
+# Find prime factors using 'LCM by division'
+# See: https://www.cuemath.com/numbers/lcm-least-common-multiple/
+prime_dividers_set = set()
+prime_numbers_list = [2]
+step_remainders = sorted(cycle_count_per_start_node.values())
+while True:
+    
+    # Do 'LCM-by-division'
+    # -> Try dividing each step count by the next highest prime & store results
+    # -> If *any* of the numbers divide, record that prime, product of these primes givens LCM answer
+    # -> Record divided step counts if divisible, otherwise do nothing, and repeat process
+    prev_prime = prime_numbers_list[-1]
+    step_dividers_list = [prev_prime if rem % prev_prime == 0 else 1 for rem in step_remainders]
+    keep_prime = any(divider > 1 for divider in step_dividers_list)
+    if keep_prime:
+        prime_dividers_set.add(prev_prime)
+        step_remainders = [rem // divider for rem, divider in zip(step_remainders, step_dividers_list)]
+    
+    # If we've divided down every step count, we're done!
+    stop_dividing = all([rem == 1 for rem in step_remainders])
+    if stop_dividing:
         break
     
-answer_2 = num_steps
+    # Find next prime number
+    for n in range(1 + prev_prime, prev_prime*prev_prime*prev_prime):
+        is_not_divisible = all([(n % each_prime) != 0 for each_prime in prime_numbers_list])
+        if is_not_divisible:
+            prime_numbers_list.append(n)
+            break
+        pass
+    pass
+
+# Multiply the primes together to get LCM
+answer_2 = 1
+for prime in prime_dividers_set:
+    answer_2 = answer_2*prime
 print("Part 2:", answer_2)
